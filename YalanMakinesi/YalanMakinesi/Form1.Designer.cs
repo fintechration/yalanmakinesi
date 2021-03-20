9902ac994963e5bd7e68e0d22b542203b3a187a1
Kaan SAHIN <eee.kaansahin@outlook.com>
2021-03-20 21:59:59 +0300

    Urunun ilk calisan hali olusturuldu, butonlar eklendi ,yes no case'ler eklendi, WI 345

---
+++ YalanMakinesi/YalanMakinesi/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace YalanMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(110, 72);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(145, 91);
+            this.btnYes.TabIndex = 0;
+            this.btnYes.Text = "EVET";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNO
+            // 
+            this.btnNO.Location = new System.Drawing.Point(351, 72);
+            this.btnNO.Name = "btnNO";
+            this.btnNO.Size = new System.Drawing.Size(145, 91);
+            this.btnNO.TabIndex = 1;
+            this.btnNO.Text = "HAYIR";
+            this.btnNO.UseVisualStyleBackColor = true;
+            this.btnNO.Click += new System.EventHandler(this.btnNO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ŞEYMA BOK MUDUR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 197);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNO);
+            this.Controls.Add(this.btnYes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNO;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
