b4ecbd9d5a171e93b0bae467f85fa7befaca526e
DESKTOP-JPLK4JM\cekicen <eee.kaansahin@yandex.com>
2021-03-20 23:34:13 +0300

    serilog loglama yapısı kuruldu , WI 235

--- YalanMakinesi/YalanMakinesi/Form1.cs
+++ YalanMakinesi/YalanMakinesi/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Serilog;
+
+
 
 namespace YalanMakinesi
 {
@@ -14,21 +17,55 @@ namespace YalanMakinesi
     {
         public Form1()
         {
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.File("logs/yalanmakinesi_.log", rollingInterval: RollingInterval.Day,
+               outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+               .CreateLogger();
+
             InitializeComponent();
+            Log.Information("--------------------------Uygulama calıstırıldı--------------------------");
+            FormClosing += Form1_FormClosing;
+          
         }
 
         private void btnYes_Click(object sender, EventArgs e)
         {
+            Log.Information("Yes butonuna tıklandı.");
             this.BackColor = Color.Green;
+            Log.Information("Arka plan rengi yesile dönüstürüldü.");
             MessageBox.Show("TEBRIKLER DOĞRU CEVAP");
-            
+            Log.Information("Mesaj kutusunda bilgi verildi");
+
+
         }
 
         private void btnNO_Click(object sender, EventArgs e)
         {
+            Log.Information("No butonuna tıklandı.");
             this.BackColor = Color.Red;
+            Log.Information("Arka plan rengi kırmızıya dönüstürüldü.");
             MessageBox.Show("MAALESEF BILEMEDINIZ :(");
-          
+            Log.Information("Mesaj kutusunda bilgi verildi");
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Log.Debug("Uygulama kapatma dügmesine basıldı.");
+            
+            if (MessageBox.Show("Çıkmak istediğinize emin misiniz?"," ",
+                MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+                Log.Information("Kapatma işlemi iptal edildi.");
+
+                // iptal ederseniz ne yapacağınızı buraya yazın
+                
+            }
+            else
+            {
+                Log.Information("Uygulama kapatıldı");
+            }
 
         }
     }
